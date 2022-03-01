fdb022b6f901b6a5ea6acd72302afa27cee75c95
Gurnoor Singh 21103010 <74511204+xamin07@users.noreply.github.com>
2022-03-02 01:16:03 +0530

    Add files via upload

---
+++ output_assignment4.py
@@ -0,0 +1,63 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/gurno/AppData/Local/Programs/Python/Python310/assignment4.py
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 3
+USING FOR LOOP
+    1 
+   1 1 
+  1 2 1 
+
+
+
+USING WHILE LOOP
+    1 
+   1 1 
+  1 2 1 
+
+
+
+USING RECURSSIONS
+    1   
+  1   1   
+1   2   1   
+
+
+ANSWER 3
+Enter 2 numbers: 5 6
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [9, 10, 5, 10, 6, 11]
+<d> Set: {5, 6, 9, 10, 11}
+<e> Immutable set: frozenset({5, 6, 9, 10, 11})
+<f> Hash value of the max value from the above set: 11
+
+
+ANSWER 4 
+Object created
+The name of the student it Gurnoor  and SID is 21103010.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
+ANSWER 6
+Enter the word: BTA
+Enter a new meaningful word using the exact same letters to test your friendship: BAT
+Does the word makes sense?(y/Y or n/N )Y
+The friends pass the friendship test
